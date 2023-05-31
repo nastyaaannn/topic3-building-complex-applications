29b1a248f185b113d3b43a87b99d1cc34a1eefef
Nastya <nastyremez8439@gmail.com>
2023-05-31 18:47:15 +0300

    AppDrawer.kt

--- app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
+++ app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -48,12 +49,12 @@ fun AppDrawer(
  */
 @Composable
 private fun AppDrawerHeader() {
-  //TODO add your code here
+  Text(
+    text = stringResource(R.string.default_username), color=MaterialTheme.colors.primaryVariant)
+  ProfileInfo()
 }
-
 @Composable
 fun ProfileInfo() {
-  //TODO add your code here
 }
 
 @Composable
